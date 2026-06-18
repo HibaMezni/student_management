9070f6821f3e90d035946afb0bd00acf91656884
Hiba MEZNI <Hiba.MEZNI@tessi.fr>
2026-06-18 19:13:51 +0100

    Fix Enrollment DTO status type

--- src/main/java/tn/esprit/studentmanagement/dto/EnrollmentDto.java
+++ src/main/java/tn/esprit/studentmanagement/dto/EnrollmentDto.java
@@ -3,7 +3,7 @@ package tn.esprit.studentmanagement.dto;
 import lombok.Getter;
 import lombok.Setter;
 import tn.esprit.studentmanagement.entities.Course;
-import tn.esprit.studentmanagement.entities.EnrollmentStatus;
+import tn.esprit.studentmanagement.entities.Status;
 import tn.esprit.studentmanagement.entities.Student;
 
 import java.time.LocalDate;
@@ -14,7 +14,7 @@ public class EnrollmentDto {
     private Long idEnrollment;
     private LocalDate enrollmentDate;
     private Double grade;
-    private EnrollmentStatus status;
+    private Status status;
     private Student student;
     private Course course;
 }
