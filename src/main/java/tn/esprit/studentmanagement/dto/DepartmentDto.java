3066fa61bc053dd6226cd21acdd2596f9c27a0db
Hiba MEZNI <Hiba.MEZNI@tessi.fr>
2026-06-18 16:53:56 +0100

    Fix Department controller DTO issue

---
+++ src/main/java/tn/esprit/studentmanagement/dto/DepartmentDto.java
@@ -0,0 +1,11 @@
+package tn.esprit.studentmanagement.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartmentDto {
+    private Long idDepartment;
+    private String name;
+}
